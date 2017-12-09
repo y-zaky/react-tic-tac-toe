ef3d59e3b7209470a5d52eaf831217e578c72d4b
Yahia Zaky <zaky20@hotmail.com>
2017-12-09 11:37:20 +0000

    Square no longer keeps its own state; it receives its value from its parent Board and informs its parent when it’s clicked. We call components like this controlled components.

--- src/index.js
+++ src/index.js
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      value: null
-   }
-  }
 
     render() {
       return (
-        <button className="square" onClick={() => this.setState({ value: this.props.value }) } >
-          {this.state.value}
+        <button className="square" onClick={() => this.props.onClick() } >
+          {this.props.value}
         </button>
       );
     }
@@ -24,13 +18,26 @@ class Square extends React.Component {
     constructor(props) {
       super(props)
       this.state = {
-        value: [null,null,null,null,null,null,null,null,null]
+        squares: [
+          null, null, null,
+          null, null, null,
+          null, null, null
+        ]
       }
 
     }
 
     renderSquare(i) {
-      return <Square value={i}/>;
+      return <Square 
+      value={this.state.squares[i]} 
+      onClick={()=>this.handleClick(i)}
+      />;
+    }
+
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = "X";
+      this.setState({squares: squares});
     }
   
     render() {
