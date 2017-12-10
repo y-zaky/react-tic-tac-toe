15399c0399df9b4496fdf401729afbde1e8d9c44
Yahia Zaky <zaky20@hotmail.com>
2017-12-10 14:19:30 +0000

    key added on newley rendered Li elements to maintain state

--- src/index.js
+++ src/index.js
@@ -85,6 +85,17 @@ function Square (props) {
       const history = this.state.history;
       const current = history[history.length - 1];
       const winner = calculateWinner(current.squares);
+
+      const moves = history.map( (step,move) => {
+        const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li  key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+      })
       
       let status;
       if (winner) {
@@ -103,7 +114,7 @@ function Square (props) {
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
