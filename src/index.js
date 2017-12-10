3b0755f221a5d16182cc2458eb40d0a6be6da87e
Yahia Zaky <zaky20@hotmail.com>
2017-12-10 14:57:39 +0000

    time travel implemented - game complete

--- src/index.js
+++ src/index.js
@@ -62,11 +62,19 @@ function Square (props) {
           squares: Array(9).fill(null),
         }],
         xIsNext: true,
+        stepNumber:0
       }
     }
 
+    jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
+      });
+    }  
+
     handleClick(i) {
-      const history = this.state.history;
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
       const current = history[history.length - 1];
       const squares = current.squares.slice();
       if (calculateWinner(squares) || squares[i]) {
@@ -77,13 +85,14 @@ function Square (props) {
         history: history.concat([{
           squares: squares,
         }]),
+        stepNumber: history.length,
         xIsNext: !this.state.xIsNext,
       });
     }
 
     render() {
       const history = this.state.history;
-      const current = history[history.length - 1];
+      const current = history[this.state.stepNumber];
       const winner = calculateWinner(current.squares);
 
       const moves = history.map( (step,move) => {
